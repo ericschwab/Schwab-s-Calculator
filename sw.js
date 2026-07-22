6f25f8197d1786de1db4c721ddf3381488e33cc4
ericschwab <67442212+ericschwab@users.noreply.github.com>
2026-07-22 13:31:07 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "rpn12c-v3";
+const CACHE = "rpn12c-v4";
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(["./", "./index.html", "./apple-touch-icon.png", "./icon-512.png"])).then(() => self.skipWaiting()));
 });
